3f24b739a84122a9c767f9bbab837eaab620ca9f
Dave Fury <david.fury@reservoir-devs.com>
2020-02-16 20:24:44 +0000

    feat: added initial packaging solution and project

---
+++ src/Templates.Generation/pack.ps1
@@ -0,0 +1,10 @@
+﻿Param(
+[Parameter(Mandatory=$true)]
+[String]
+$configurationState,
+[Parameter(Mandatory=$true)]
+[String]
+$outputDirectory
+)
+
+dotnet pack Templates.Generation.csproj -c $configurationState --no-build --no-restore -o $outputDirectory
